8c65074ec23f2ecb276ce92897e3a6255a7e173a
meghlal <meghlal.khan@gmail.com>
2015-09-28 02:55:50 +0530

    PHP file update for the advance json
    PHP file update for the advance json array and object. For commit "Get
    method with advance json "

--- Server File/android/get/index.php
+++ Server File/android/get/index.php
@@ -1,5 +1,11 @@
 <?php
-$a=array();
-$a['name']="Meghlal Khan";
+$name=$a=array();
+
+$name[0]['fname']="Meghlal";
+$name[0]['lname']="Khan";
+$name[1]['fname']="Bappa";
+$name[1]['lname']="Khan";
+
+$a['name']=$name;
 $a['tol']=$_GET['num1'] + $_GET['num2'];
 echo json_encode($a);
